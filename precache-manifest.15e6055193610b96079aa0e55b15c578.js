c8421ce0a99e8d0841b040a42e3e12f20f10675c
Josh Guerrero <jrg99654@gmail.com>
2019-06-09 00:30:14 -0800

    Updates

---
+++ precache-manifest.15e6055193610b96079aa0e55b15c578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7bb323d921fbb70873f1230f646fb2c",
+    "url": "/react-face-recognition-brain/index.html"
+  },
+  {
+    "revision": "26a126d2928aa2d3e2e2",
+    "url": "/react-face-recognition-brain/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ff1db4634bf45dc6873b",
+    "url": "/react-face-recognition-brain/static/css/main.593682ac.chunk.css"
+  },
+  {
+    "revision": "26a126d2928aa2d3e2e2",
+    "url": "/react-face-recognition-brain/static/js/2.55705f0d.chunk.js"
+  },
+  {
+    "revision": "ff1db4634bf45dc6873b",
+    "url": "/react-face-recognition-brain/static/js/main.accf8cc0.chunk.js"
+  },
+  {
+    "revision": "44cc3dd7d1f0122baacb",
+    "url": "/react-face-recognition-brain/static/js/runtime~main.8914d0ed.js"
+  }
+]);
